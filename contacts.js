7e3e164a23cf867c1c09693468ff48b0d7291eb2
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-14 11:03:37 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -3,21 +3,37 @@ const fs = require("fs").promises;
 
 const contactsPath = path.resolve("./db/contacts.json");
 
-console.log(contactsPath);
-
-function listContacts() {
-  fs.readFile(contactsPath)
-    .then((data) => console.log(data.toString()))
-    .catch((err) => console.log(err.message));
+async function listContacts() {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    const array = JSON.parse(contacts);
+    return array;
+  } catch {
+    (err) => console.log(err.message);
+  }
 }
 
-function getContactById(contactId) {
-  fs.readFile("./db/contacts.json").then((data) => console.log(data.toString()))
+async function getContactById(contactId) {
+  try {
+    const array = await listContacts();
+    const contactById = array.find((obj) => obj.id === contactId);
+    return contactById;
+  } catch {
+    (err) => console.log(err.message);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+  try {
+    const array = await listContacts();
+    const newArray = array.filter((item) => item.id !== contactId);
+    console.log(newArray);
+    return newArray;
+  } catch {
+    (err) => console.log(err.message);
+  }
 }
+removeContact("10");
 
 function addContact(name, email, phone) {
   // ...твій код
